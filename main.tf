c9059d1af10616334c2e9390b48d83744c1706b6
Zach <zacharybeeler@gmail.com>
2022-04-25 21:39:35 -0500

    Adding in the instance

--- main.tf
+++ main.tf
@@ -2,3 +2,21 @@ provider "google" {
   project = "gcp-terraform-348402"
   region  = "us-central1"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
